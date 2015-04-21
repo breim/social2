6aab15b268a25d0e970768cc99b241b7afed6943
Henrique Breim <henrique@breim.com.br>
2015-04-21 19:52:48 -0300

    Add devise permitted params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  protected
+  	# Sanitized fields for devise
+	  before_filter :configure_devise_params, if: :devise_controller?
+		def configure_devise_params
+			devise_parameter_sanitizer.for(:sign_up) do |u|
+				u.permit(:name,:lastname, :email,:password,:password_confirmation)
+			end
+			devise_parameter_sanitizer.for(:account_update) do |u|
+				u.permit(:name,:lastname,:email,:password,:image,:image_file_name, :image_content_type, :image_file_size)
+			end
+			devise_parameter_sanitizer.for(:sign_in) do |u|
+				u.permit(:email,:password)
+			end
+		end
+
 end
