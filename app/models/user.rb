489184af44baa6fa48ce8e755dd18f9ba64b678c
Henrique Breim <henrique@breim.com.br>
2015-04-21 19:37:37 -0300

    Add friendly id to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+
+  # Friendly id Gem
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
+  def slug_candidates
+    [
+      :name,
+      [:name, :lastname],
+      [:name, :lastname, :birthday_day]
+    ]
+  end
+  
 end
